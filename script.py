73677fc2c5a3b3f7f50f8bff642f5a8339566f27
Mortada <moroelharrak@gmail.com>
2022-05-11 15:14:37 +0100

    added CSV bulk mailing. requires a CSV file containing only emails and full name as header. and an HTML file to send as email body.

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import csv
 import smtplib
 import ssl
 from datetime import datetime
@@ -12,30 +13,25 @@ context = ssl.create_default_context()
 server = None
 
 
-def textMessage(innerMessage: str):
+def textMessage(subj: str, sender: str, receiver: str, innerMessage: str):
     return f"""\
-From: {senderEmail}
-To: {receiverEmail}
-Subject: {subject}
+From: {sender}
+To: {receiver}
+Subject: {subj}
 Date: {datetime.now().strftime("%d/%m/%Y %H:%M")}
 
 {innerMessage}\n"""
 
 
-def htmlMessage(text: str):
+def htmlMessage(subj: str, sender: str, receiver: str, text: str, htmlPath: str):
     innerMessage = MIMEMultipart("alternative")
-    innerMessage["Subject"] = subject
-    innerMessage["From"] = senderEmail
-    innerMessage["To"] = receiverEmail
+    innerMessage["Subject"] = subj
+    innerMessage["From"] = sender
+    innerMessage["To"] = receiver
     innerMessage["Date"] = datetime.now().strftime("%d/%m/%Y %H:%M")
-    innerMessage.attach(MIMEText(f"""\
-{text}""", "plain"))
-    innerMessage.attach(MIMEText(f"""\
-<html>
-    <body>
-        <p>{text}</p>
-    </body>
-</html>""", "html"))
+    innerMessage.attach(MIMEText(f"\n{text}", "plain"))
+    with open(htmlPath) as file:
+        innerMessage.attach(MIMEText(file.read(), "html"))
     return innerMessage.as_string()
 
 
@@ -66,21 +62,34 @@ if __name__ == '__main__':
             except Exception as e:
                 print("\nUsername and Password does not match.\n")
         print("\nLogged in successfully.\n")
-        while menuRes != '2':
+        while menuRes != '3':
             menuRes = input("""\
 1 ==> Send an email.
-2 ==> Quit.
+2 ==> Send multiple emails.
+3 ==> Quit.
 
 response <== """)
             match menuRes:
                 case '1':
                     receiverEmail = input("\nRecipient :")
                     subject = input("Subject :")
-                    # message = textMessage(input("Message :"))
-                    message = htmlMessage(input("Message :"))
+                    # message = textMessage(subject, senderEmail, receiverEmail, input("Message :"))
+                    message = htmlMessage(subject, senderEmail, receiverEmail, input("Message :"), input("HTML path :"))
                     server.sendmail(senderEmail, receiverEmail, message)
                     print("\nYour message has been sent successfully.\n")
                 case '2':
+                    subject = input("Subject :")
+                    # message = textMessage(input("Message :"))
+                    message = input("Message :")
+                    html = input("HTML :")
+                    with open(input("CSV path :")) as f:
+                        reader = csv.reader(f)
+                        next(reader)
+                        for email, fullName in reader:
+                            print(f"\nSending email to {fullName}.")
+                            server.sendmail(senderEmail, email, htmlMessage(subject, senderEmail, email, message, html))
+                    print("\nAll emails has been sent successfully.\n")
+                case '3':
                     pass
                 case _:
                     print("\nplease choose either 1 or 2.\n")
