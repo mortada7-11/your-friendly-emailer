3fb2478ea44fcc48036ea3136b78611d5afb39dc
Mortada <moroelharrak@gmail.com>
2022-05-11 12:46:32 +0100

    Added HTML support using MIME protocol.

--- script.py
+++ script.py
@@ -2,12 +2,41 @@ import smtplib
 import ssl
 from datetime import datetime
 from getpass import getpass
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 smtpServer = "smtp.gmail.com"
 sslPort = 465
 tlsPort = 587
 context = ssl.create_default_context()
 server = None
+
+
+def textMessage(innerMessage: str):
+    return f"""\
+From: {senderEmail}
+To: {receiverEmail}
+Subject: {subject}
+Date: {datetime.now().strftime("%d/%m/%Y %H:%M")}
+
+{innerMessage}\n"""
+
+
+def htmlMessage(text: str):
+    innerMessage = MIMEMultipart("alternative")
+    innerMessage["Subject"] = subject
+    innerMessage["From"] = senderEmail
+    innerMessage["To"] = receiverEmail
+    innerMessage["Date"] = datetime.now().strftime("%d/%m/%Y %H:%M")
+    innerMessage.attach(MIMEText(f"""\
+{text}""", "plain"))
+    innerMessage.attach(MIMEText(f"""\
+<html>
+    <body>
+        <p>{text}</p>
+    </body>
+</html>""", "html"))
+    return innerMessage.as_string()
 
 
 def sslMode():
@@ -25,6 +54,7 @@ def tlsMode():
 if __name__ == '__main__':
     try:
         menuRes = '0'
+        # server = sslMode()
         server = tlsMode()
         logRes = None
         senderEmail = ""
@@ -46,13 +76,8 @@ response <== """)
                 case '1':
                     receiverEmail = input("\nRecipient :")
                     subject = input("Subject :")
-                    message = f"""\
-From: {senderEmail}
-To: {receiverEmail}
-Subject: {subject}
-Date: {datetime.now().strftime( "%d/%m/%Y %H:%M" )}
-
-{input('Message: ')}\n"""
+                    # message = textMessage(input("Message :"))
+                    message = htmlMessage(input("Message :"))
                     server.sendmail(senderEmail, receiverEmail, message)
                     print("\nYour message has been sent successfully.\n")
                 case '2':
